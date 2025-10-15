20a8456d0ee6cc8677d47ccc6d1817e89933836a
whitefri2z <white.fri2z12@gmail.com>
2025-10-15 16:01:00 +0200

    Beginning cpp horse

---
+++ Source/GachaHorse/Private/CharactersData/BaseHorse.cpp
@@ -0,0 +1,353 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharactersData/BaseHorse.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+// ==========================
+	// ==    Base Functions    ==
+	// ==========================
+
+ABaseHorse::ABaseHorse()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// PREPARE COMPONENTS
+	// SPRING ARM
+
+	HorseSpringArm = CreateDefaultSubobject<USpringArmComponent>("Spring Arm");
+	HorseSpringArm->SetupAttachment(GetCapsuleComponent());
+	HorseSpringArm->TargetArmLength = 600.0f;
+
+	// CAMERA
+	
+	HorseCamera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	HorseCamera->SetupAttachment(HorseSpringArm);
+
+	// FUNCTIONS
+
+	GetMesh()->OnComponentHit.AddDynamic(this, &ABaseHorse::OnHit);
+}
+
+void ABaseHorse::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// FIRST : PREPARE RESPAWN
+
+	SetPlayerRespawn();
+
+	// SECOND : SET ALL SPEED VALUES
+
+	InitAcceleration();
+	InitSpeed();
+	InitHandling();
+
+	// THIRD : SET SPEED
+
+	SetTargetSpeed(1);
+
+	// FOURTH : CREATE WIDGET
+
+	CreateWidgetRace();
+}
+
+void ABaseHorse::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// ALWAYS CHECK SLOPE
+	SlopeCheck();
+
+	// CHARGE JUMP
+	ChargeJump(DeltaTime);
+
+	// MOVE HORSEY FORWARD
+	if (!bIsRagdoll && GetCharacterMovement()->IsMovingOnGround())
+		AddMovementInput(GetActorForwardVector(), 0.5 * SlopeType + CurrentSpeed <= TargetSpeed * (1 + SlopeType * 0.15) ? 1 : -1, false);
+
+	// MOVE HORSEY LEFTY RIGHTY
+	if (!(-0.1 < SideSpeed && SideSpeed < 0.1))
+		AddMovementInput(GetActorRightVector(), SideSpeed < 0 ? 2 : -2);
+
+	// LANDING BEHAVIOUR
+	if (bHasJustJumped)
+	{
+		float NewHalfHeight = FMath::FInterpTo(GetCapsuleComponent()->GetUnscaledCapsuleHalfHeight() - 34, 46, DeltaTime, 92);
+
+		if (NewHalfHeight >= 46)
+		{
+			bHasJustJumped = false;
+			NewHalfHeight = 46;
+		}
+		GetCapsuleComponent()->SetCapsuleSize(34, NewHalfHeight + 34, true);
+	}
+
+	
+	// GAME END :
+
+	if (bGameEnded)
+	{
+		HorseSpringArm->TargetArmLength = FMath::FInterpTo(HorseSpringArm->TargetArmLength, 200, DeltaTime, 200);
+		HorseSpringArm->SetRelativeRotation(FRotator(0, -20, FMath::FInterpTo(HorseSpringArm->GetRelativeRotation().Roll, -270, DeltaTime, 135)));
+	}
+}
+
+void ABaseHorse::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+	// =========================
+	// ==       Respawn       ==
+	// =========================
+
+void ABaseHorse::SetPlayerRespawn()
+{
+	RespawnPoint = GetActorTransform();
+}
+
+void ABaseHorse::SetPlayerRespawn(const FTransform& RespawnLoc)
+{
+	RespawnPoint = RespawnLoc;
+}
+
+	// =========================
+	// ==        Speed        ==
+	// =========================
+
+void ABaseHorse::InitAcceleration()
+{
+	GetCharacterMovement()->MaxAcceleration = 300 + (20 * Stats[0]);
+}
+
+void ABaseHorse::InitSpeed()
+{
+	SpeedTable[5] = 1750 + (Stats[1] * 25);
+	GetCharacterMovement()->MaxWalkSpeed = SpeedTable[5];
+}
+
+void ABaseHorse::InitHandling()
+{
+	TurnRateFactor = 0.05 + (Stats[2] * 0.005);
+}
+
+void ABaseHorse::ResetShiftSpeed()
+{
+	bCanShiftSpeed = true;
+}
+
+void ABaseHorse::SetTargetSpeed(int IndexSpeed)
+{
+	TargetSpeed = SpeedTable[IndexSpeed];
+}
+
+void ABaseHorse::SlopeCheck()
+{
+	// ONLY DOES THIS IF YOU'RE ON THE GROUND
+	if (!GetCharacterMovement()->IsMovingOnGround())
+	{
+		SlopeType = 0;
+		return;
+	}
+
+	// LINE TRACE TO THE GROUND
+	FCollisionQueryParams RV_TraceParams = FCollisionQueryParams(FName(TEXT("RV_Trace")), true, this);
+	RV_TraceParams.bTraceComplex = true;
+	RV_TraceParams.bReturnPhysicalMaterial = false;
+
+	//Re-initialize hit info
+	FHitResult RV_Hit(ForceInit);
+	
+	//call GetWorld() from within an actor extending class
+	GetWorld()->LineTraceSingleByChannel(
+		RV_Hit,
+		GetActorLocation(),
+		GetActorLocation() + FVector(0,0,-200),
+		ECC_Pawn,
+		RV_TraceParams);
+
+	float SlopeTypeCalculation = FVector::DotProduct(RV_Hit.ImpactNormal, GetActorForwardVector());
+
+	// NEARLY EQUAL TO 0?
+	if (-0.0001 < SlopeTypeCalculation && SlopeTypeCalculation < 0.0001)
+	{
+		SlopeType = 0;
+	}
+	else
+	{
+		SlopeType = SlopeTypeCalculation > 0 ? 1 : -1;
+	}
+}
+
+void ABaseHorse::CalculateCurrentSpeed()
+{
+	CurrentSpeed = FVector2D::DotProduct(FVector2D(GetCharacterMovement()->Velocity.X, GetCharacterMovement()->Velocity.Y), FVector2D(GetActorForwardVector().X, GetActorForwardVector().Y));
+	SideSpeed = FVector2D::DotProduct(FVector2D(GetCharacterMovement()->Velocity.X, GetCharacterMovement()->Velocity.Y), FVector2D(GetActorRightVector().X, GetActorRightVector().Y));
+}
+
+	// ========================
+	// ==        Jump        ==
+	// ========================
+
+void ABaseHorse::ChargeJump(float DeltaTime)
+{
+	// DON'T WASTE TIME IF YOU'RE READY TO JUMP
+	if (!bIsChargingJump || JumpCharge == 1.0f)
+		return;
+
+	// MAKE SURE YOU CAN'T GO HIGHER THAN 100%
+	if (JumpCharge > 1.0f)
+	{
+		JumpCharge = 1.0f;
+		return;
+	}
+
+	// APPLY PERCENTAGE CALC
+	JumpCharge += FMath::Clamp(JumpCharge + (DeltaTime / 3), 0.0f, 1.0f);
+
+	// MAKE SURE TO CHANGE JUMP VELOCITY ONLY IF YOU CAN JUMP
+	if (JumpCharge < 0.7)
+		return;
+
+	// CHANGE JUMP Z VELOCITY
+	GetCharacterMovement()->JumpZVelocity = 450 * JumpCharge;
+}
+
+void ABaseHorse::Landed(const FHitResult& Hit)
+{
+	Super::Landed(Hit);
+
+	bHasJustJumped = true;
+}
+
+	// ==========================
+	// ==       End Game       ==
+	// ==========================
+
+void ABaseHorse::FinishRace()
+{
+	SetTargetSpeed(1);
+	CreateWidgetFinish();
+}
+	
+	// =========================
+	// ==       Respawn       ==
+	// =========================
+
+void ABaseHorse::Respawn()
+{
+	if (bIsRagdoll)
+		CeaseRagdoll();
+	
+	SetActorLocation(RespawnPoint.GetLocation());
+	GetController()->SetControlRotation(FRotator(RespawnPoint.GetRotation()));
+}
+
+
+	// =========================
+	// ==       Ragdoll       ==
+	// =========================
+
+void ABaseHorse::BeginRagdoll()
+{
+	bIsRagdoll = true;
+	GetMesh()->SetCollisionEnabled(ECollisionEnabled::Type::QueryAndPhysics);
+	GetMesh()->SetSimulatePhysics(true);
+	GetCharacterMovement()->StopMovementImmediately();
+
+	GetWorldTimerManager().SetTimer(
+		RagdollTimerHandle,
+		this,
+		&ABaseHorse::Respawn,
+		1.0f,
+		false);
+}
+ 
+void ABaseHorse::CeaseRagdoll()
+{
+	bIsRagdoll = false;
+	GetMesh()->SetCollisionEnabled(ECollisionEnabled::Type::QueryOnly);
+	GetMesh()->SetSimulatePhysics(false);
+	GetMesh()->AttachToComponent(GetCapsuleComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, -90), FRotator(0, 0, -90));
+}
+	
+	// ========================
+	// ==       Damage       ==
+	// ========================
+
+void ABaseHorse::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (FVector2D(Hit.ImpactNormal.X, Hit.ImpactNormal.Y).Length() < 0.35)
+		return;
+		
+	FVector2D NormalHitSave2D = FVector2D(Hit.ImpactNormal.X, Hit.ImpactNormal.Y);
+	FVector2D ForwardVector2D = FVector2D(GetActorForwardVector().X, GetActorForwardVector().Y);
+	FVector2D RightVector2D = FVector2D(GetActorRightVector().X, GetActorRightVector().Y);
+
+	
+	if (FVector2D::DotProduct(NormalHitSave2D, ForwardVector2D) < -0.5)
+	{
+		// HEADSHOT!
+		if (GetCharacterMovement()->IsMovingOnGround())
+		{
+			if (CurrentSpeed > 800)
+			{
+				BeginRagdoll();
+				SetTargetSpeed(1);
+			}
+			else
+			{
+				GetCharacterMovement()->StopMovementImmediately();
+				SetTargetSpeed(FMath::Clamp(TargetSpeed, 0, 1));
+			}
+		}
+		else
+		{
+			BeginRagdoll();
+			SetTargetSpeed(1);
+		}
+	}
+	else if (FVector2D::DotProduct(NormalHitSave2D, ForwardVector2D) <= 0.0)
+	{
+		// SIDE HIT
+		AddControllerYawInput(FVector2D::DotProduct(NormalHitSave2D, RightVector2D) > 0 ? 0.5 : -0.5);
+
+		GetCharacterMovement()->Velocity = GetCharacterMovement()->Velocity.RotateAngleAxis(FVector2D::DotProduct(NormalHitSave2D, RightVector2D) > 0 ? 0.5 : -0.5, FVector(0,0,1));
+
+		if (CurrentSpeed > 1000)
+		{
+			SetTargetSpeed(FMath::Clamp(TargetSpeedIndex, 0, 4));
+			bCanShiftSpeed = false;
+			GetWorldTimerManager().SetTimer(
+				ShiftSpeedTimerHandle,
+				this,
+				&ABaseHorse::ResetShiftSpeed,
+				1.0,
+				false);
+		}
+	}
+	else
+	{
+		// BACK HIT
+	}
+}
+
+	// =========================
+	// ==       Widgets       ==
+	// =========================
+
+void ABaseHorse::CreateWidgetRace_Implementation(){}
+
+void ABaseHorse::DeleteWidgetRace_Implementation(){}
+
+void ABaseHorse::CreateWidgetFinish_Implementation(){}
+ 
+void ABaseHorse::DeleteWidgetFinish_Implementation(){}
---
+++ Source/GachaHorse/Public/CharactersData/BaseHorse.h
@@ -0,0 +1,236 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseHorse.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class GACHAHORSE_API ABaseHorse : public ACharacter
+{
+	GENERATED_BODY()
+
+	// UPROPERTIES
+public:
+	// =========================
+	// ==      Movements      ==
+	// =========================
+
+protected:
+	// ==========================
+	// ==      Components      ==
+	// ==========================
+
+	UPROPERTY(EditAnywhere, Category="Components")
+	TObjectPtr<USpringArmComponent> HorseSpringArm;
+	
+	UPROPERTY(EditAnywhere, Category="Components")
+	TObjectPtr<UCameraComponent> HorseCamera;
+	
+	// =========================
+	// ==        Stats        ==
+	// =========================
+
+	/**
+	 * Array with three ints. \n
+	 * First Stat : Acceleration \n
+	 * Second Stat : Speed \n
+	 * Third Stat : Handling
+	 */
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Stats")
+	TArray<int> Stats = {2,2,2};
+	
+	// =========================
+	// ==        Speed        ==
+	// =========================
+
+	// TARGETS
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Speed Targets")
+	float TargetSpeed = 0.0f;
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Speed Targets")
+	int TargetSpeedIndex = 0; // WILL PROBABLY BE REMOVED
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Speed Targets")
+	TArray<float> SpeedTable = {-200, 0, 350, 800, 1200, 1750};
+
+	// ACTUAL
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Speed Actual")
+	bool bCanShiftSpeed = true;
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Speed Actual")
+	FTimerHandle ShiftSpeedTimerHandle;
+	
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Speed Actual")
+	float CurrentSpeed = 0.0f;
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Speed Actual")
+	float SideSpeed = 0.0f;
+
+	// SLOPE
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Slope")
+	int SlopeType;
+	
+	// ==========================
+	// ==         Jump         ==
+	// ==========================
+	
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Jump")
+	bool bIsChargingJump = false;
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Jump")
+	float JumpCharge = 0.0f;
+	
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Jump")
+	bool bHasJustJumped = false;
+	
+	// ==========================
+	// ==   Lateral movement   ==
+	// ==========================
+
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Lateral Movement")
+	float TurnRateFactor = 0.0f;
+	
+	// =========================
+	// ==       Respawn       ==
+	// =========================
+
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Respawn")
+	FTransform RespawnPoint;
+	
+	// =========================
+	// ==       Ragdoll       ==
+	// =========================
+
+	/**
+	 * As you can see, the above property is a boolean that allows one to know whether or
+	 * not the character is currently in the process of being a ragdoll.
+	 * In truth, this is an incredibly necessary and important, core function of the
+	 * game ; without the ragdoll, it would be like Mario without Luigi, spaghetti
+	 * without Ketchup, or indeed, Aerith without a sword sticking through her dumb
+	 * fucking chest.
+	 */
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Ragdoll")
+	bool bIsRagdoll;
+	
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="Ragdoll")
+	FTimerHandle RagdollTimerHandle;
+	
+	// ==========================
+	// ==       End Game       ==
+	// ==========================
+	
+	UPROPERTY(Blueprintable, BlueprintReadWrite, Category="EndGame")
+	bool bGameEnded;
+	
+
+	// UFUNCTIONS
+public:	
+	// ==========================
+	// ==    Base Functions    ==
+	// ==========================
+	
+	ABaseHorse();
+	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	// =========================
+	// ==        Speed        ==
+	// =========================
+
+	UFUNCTION()
+	void SetTargetSpeed(int IndexSpeed);
+	
+	// ==========================
+	// ==       End Game       ==
+	// ==========================
+
+	UFUNCTION()
+	void FinishRace();
+	
+protected:
+	// ==========================
+	// ==    Base Functions    ==
+	// ==========================
+	
+	virtual void BeginPlay() override;
+	
+	// =========================
+	// ==       Respawn       ==
+	// =========================
+
+	UFUNCTION()
+	void SetPlayerRespawn();
+	void SetPlayerRespawn(const FTransform& RespawnLoc);
+	
+	// =========================
+	// ==        Speed        ==
+	// =========================
+
+	UFUNCTION()
+	void InitAcceleration();
+
+	UFUNCTION()
+	void InitSpeed();
+
+	UFUNCTION()
+	void InitHandling();
+
+	UFUNCTION()
+	void ResetShiftSpeed();
+
+	UFUNCTION()
+	void SlopeCheck();
+
+	UFUNCTION()
+	void CalculateCurrentSpeed();
+	
+	// ==========================
+	// ==         Jump         ==
+	// ==========================
+
+	UFUNCTION()
+	void ChargeJump(float DeltaTime);
+
+	void Landed(const FHitResult& Hit) override;
+	
+	// =========================
+	// ==       Respawn       ==
+	// =========================
+
+	UFUNCTION()
+	void Respawn();
+	
+	// =========================
+	// ==       Ragdoll       ==
+	// =========================
+
+	UFUNCTION()
+	void BeginRagdoll();
+	UFUNCTION()
+	void CeaseRagdoll();
+	
+	// ========================
+	// ==       Damage       ==
+	// ========================
+	
+	UFUNCTION()    
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
+	// =========================
+	// ==       Widgets       ==
+	// =========================
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void CreateWidgetRace();
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void DeleteWidgetRace();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void CreateWidgetFinish();
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void DeleteWidgetFinish();
+	
+};
