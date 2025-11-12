07f7ac0c61a67baa553563d73586107153111e51
whitefri2z <white.fri2z12@gmail.com>
2025-11-12 11:35:17 +0100

    Added equipment datat table

--- Source/GachaHorse/Public/GameData/AllStructs.h
+++ Source/GachaHorse/Public/GameData/AllStructs.h
@@ -84,6 +84,73 @@ public:
 	}
 };
 
+
+USTRUCT(BlueprintType)
+struct GACHAHORSE_API FEquipDataStruct : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	// CARD DATA
+	UPROPERTY(BlueprintReadOnly, Blueprintable)
+	bool bEquipmentPossessed;
+	UPROPERTY(BlueprintReadOnly, Blueprintable)
+	int Level;
+
+	// DATA
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 EquipmentID;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Rarity;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString EquipmentName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UTexture> EquipmentPicture;
+
+	// Stats
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<int32> AccelerationPerLevel;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<int32> SpeedPerLevel;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<int32> HandlingPerLevel;
+
+	// MESH
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UMaterial> EquipMaterial;
+
+	FEquipDataStruct()
+	{
+		bEquipmentPossessed = false;
+		Level = 1;
+		EquipmentID = 0;
+		Rarity = 0;
+		EquipmentName = "ThisIsNotASaddle";
+		EquipmentPicture = nullptr;
+		AccelerationPerLevel = {0,0,0,0,0,0};
+		SpeedPerLevel= {0,0,0,0,0,0};
+		HandlingPerLevel= {0,0,0,0,0,0};
+		EquipMaterial = nullptr;
+	}
+
+	FEquipDataStruct(bool PossessEquip, int NewLevel, int32 NewID, int32 NewRarity,
+		FString NewName, TSoftObjectPtr<UTexture> NewEquipPic,
+		TArray<int32> NewAccelerationLevelArray, TArray<int32> NewSpeedLevelArray,
+		TArray<int32> NewHandlingLevelArray, TSoftObjectPtr<UMaterial> NewEquipMat)
+	{
+		bEquipmentPossessed = PossessEquip;
+		Level = NewLevel;
+		EquipmentID = NewID;
+		Rarity = NewRarity;
+		EquipmentName = NewName;
+		EquipmentPicture = NewEquipPic;
+		AccelerationPerLevel = NewAccelerationLevelArray;
+		SpeedPerLevel = NewSpeedLevelArray;
+		HandlingPerLevel = NewHandlingLevelArray;
+		EquipMaterial = NewEquipMat;
+	}
+};
+
 class GACHAHORSE_API AllStructs
 {
 public:
